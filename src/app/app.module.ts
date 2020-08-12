6987ac456d65407b4d5334750c0b2ffa004a1435
Gabriel Quispe <gabrielomarquisperosas@gmail.com>
2020-08-11 23:07:12 -0500

    integracion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterMedicoComponent } from './avanzado/router-medico/router-medico.component';
+import { NavbarComponent } from './avanzado/navbar/navbar.component';
+import { IncrementadorComponent } from './avanzado/incrementador/incrementador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouterMedicoComponent,
+    NavbarComponent,
+    IncrementadorComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
