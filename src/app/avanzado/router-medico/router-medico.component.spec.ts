6d8ad4963dc9caaaacc9a188ab15d8a5f46527fe
Gabriel Quispe Rosas <gabriel.omar.quispe.rosas@everis.com>
2020-08-14 18:30:13 -0500

    correccion

--- src/app/avanzado/router-medico/router-medico.component.spec.ts
+++ src/app/avanzado/router-medico/router-medico.component.spec.ts
@@ -1,78 +1,76 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { RouterMedicoComponent } from './router-medico.component';
-// import { Router, ActivatedRoute } from '@angular/router';
-// import 'rxjs/add/observable/empty';
-// import { Observable, Subject } from 'rxjs';
+import { RouterMedicoComponent } from './router-medico.component';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 
-// class FakeRouter {
-//   navigate( params ) { }
-// }
+class FakeRouter {
+  navigate( params ) { }
+}
 
-// class FakeActivatedRoute {
-//   // params: Observable<any> = Observable.empty();
+class FakeActivatedRoute {
+  // params: Observable<any> = Observable.empty();
 
-//   private subject = new Subject();
+  private subject = new Subject();
 
-//   push( valor ) {
-//     this.subject.next( valor );
-//   }
+  push( valor ) {
+    this.subject.next( valor );
+  }
 
-//   get params() {
-//     return this.subject.asObservable();
-//   }
+  get params() {
+    return this.subject.asObservable();
+  }
 
-// }
-
-
-// describe('RouterMedicoComponent', () => {
-//   let component: RouterMedicoComponent;
-//   let fixture: ComponentFixture<RouterMedicoComponent>;
-
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ RouterMedicoComponent ],
-//       providers: [
-//         { provide: Router, useClass: FakeRouter },
-//         { provide: ActivatedRoute, useClass: FakeActivatedRoute }
-//       ]
-//     })
-//     .compileComponents();
-//   }));
-
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(RouterMedicoComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+}
 
 
-//   it( 'Debe de redireccionar a Médico cuando se guarde', () => {
+describe('RouterMedicoComponent', () => {
+  let component: RouterMedicoComponent;
+  let fixture: ComponentFixture<RouterMedicoComponent>;
 
-//     const router = TestBed.get(Router);
-//     const spy = spyOn( router, 'navigate' );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RouterMedicoComponent ],
+      providers: [
+        { provide: Router, useClass: FakeRouter },
+        { provide: ActivatedRoute, useClass: FakeActivatedRoute }
+      ]
+    })
+    .compileComponents();
+  }));
 
-//     component.guardarMedico();
-
-//     expect( spy ).toHaveBeenCalledWith( ['medico', '123'] );
-
-
-//   });
-
-
-//   it( 'Debe de colocar el id = nuevo', () => {
-
-//     component = fixture.componentInstance;
-
-//     const activatedRoute: FakeActivatedRoute = TestBed.get(ActivatedRoute);
-
-//     activatedRoute.push( { id: 'nuevo' } );
-
-//     expect( component.id ).toBe('nuevo');
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RouterMedicoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
 
-//   });
+  it( 'Debe de redireccionar a Médico cuando se guarde', () => {
+
+    const router = TestBed.get(Router);
+    const spy = spyOn( router, 'navigate' );
+
+    component.guardarMedico();
+
+    expect( spy ).toHaveBeenCalled();
+    expect( spy ).toHaveBeenCalledWith( ['medico', '123'] );
+  });
+
+
+  it( 'Debe de colocar el id = nuevo', () => {
+
+    component = fixture.componentInstance;
+
+    const activatedRoute: FakeActivatedRoute = TestBed.get(ActivatedRoute);
+
+    activatedRoute.push( { id: 'nuevo' } );
+
+    expect( component.id ).toBe('nuevo');
+
+
+  });
 
 
 
-// });
+});
